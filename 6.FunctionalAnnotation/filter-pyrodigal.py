54993ac7d51ec04a2ecf58a1c17e91e02af65ade
Jun Hyung Cha <91577615+jhCha1994@users.noreply.github.com>
2025-09-06 02:07:37 +0900

    Update filter-pyrodigal.py

--- 6.FunctionalAnnotation/filter-pyrodigal.py
+++ 6.FunctionalAnnotation/filter-pyrodigal.py
@@ -21,7 +21,7 @@ for headers in name2seq:
 	sequence = ''.join(name2seq[headers]).replace('*','') 
 	sequence_length = len(sequence) 
 	new_header_for_me = sys.argv[1] + '|smORF|' + str(n)	  
-	if sequence_length >= 15 and sequence_length <= 101: 
+	if sequence_length >= 15 and sequence_length < 101: 
 		outf.write('>' + new_header_for_me + '\n') 
 		outf.write(sequence + '\n') 
 		n += 1
