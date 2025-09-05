f4cae1c00f83ef96134e75f358d50c0f468b16c8
Jun Hyung Cha <91577615+jhCha1994@users.noreply.github.com>
2025-09-06 02:05:37 +0900

    Update pyrodigal.sh

--- 6.FunctionalAnnotation/pyrodigal.sh
+++ 6.FunctionalAnnotation/pyrodigal.sh
@@ -4,5 +4,4 @@ cat ${1} | while read line
 	do
 	pyrodigal -i non_red_genome/${line}.fna -a 0_pyrodigal/${line}.faa --min-gene 33 --min-edge-gene 33 --max-overlap 33
 	./filter-pyrodigal.py ${line} 
-	rm 0_pyrodigal/${line}.faa 
 done 
